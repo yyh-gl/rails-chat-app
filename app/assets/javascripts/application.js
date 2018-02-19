52cf8803664f696dd4b092622c813d3b12df8c44
yy_h <yhonda@mikilab.doshisha.ac.jp>
2018-02-20 00:11:19 +0900

    Refactoring

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery
-//= require jquery_ujs
-//= require jquery.turbolinks
+//= require rails-ujs
 //= require turbolinks
 //= require_tree .
